d80cdfc94a6ce6cbcdd1a2ed40179c200f5d7c2a
MahendraSH <maahi2001sh@gmail.com>
2024-03-05 15:08:32 +0530

    rendor.com

--- node-app/index.js
+++ node-app/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 // routes imports
 import userRoutes from "./routes/userRoutes.js";
-import fomrDataRotues from "./routes/formDataRoutes.js";
+import fromDataRoutes from "./routes/formDataRoutes.js";
 import dataTableRoutes from "./routes/dataTableRoutes.js";
 import uiRoutes from "./routes/uiRoutes.js";
 import path from "path";
@@ -13,13 +13,12 @@ app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
 // dotenv path
 
-dotenv.config({ path: "./.env" });
 
 // middlewares
 
 //  Routes
 app.use("/api/user", userRoutes);
-app.use("/api/data/form", fomrDataRotues);
+app.use("/api/data/form", fromDataRoutes);
 app.use("api/data/table", dataTableRoutes);
 app.use("api/ui", uiRoutes);
 // Serve static files from the 'dist' directory
